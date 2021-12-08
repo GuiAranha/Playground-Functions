fbf4c1ecc27e71713953018d875517497c31d2c7
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2021-12-08 19:18:16 -0300

    Requisito 11 - ok

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,8 +20,38 @@ function techList(listTec, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numbers) {
+  let telNumber;
+
+  if (numbers.length != 11){
+    return "Array com tamanho incorreto.";
+  }else{
+    for (index = 0; index < numbers.length; index+=1){
+      if (numbers[index] < 0 || numbers[index] > 9){
+        return "não é possível gerar um número de telefone com esses valores";
+      }else{
+        let count = 0;
+        for (j = 0; j < numbers.length; j+=1){
+          if (numbers[index] === numbers[j]){
+            count += 1;
+            if (count == 3){
+              return "não é possível gerar um número de telefone com esses valores"
+            }
+          }
+        }
+        if (index === 0){
+          telNumber = "(" + numbers[index];
+        }else if (index === 1){
+          telNumber = telNumber + numbers[index] + ") ";
+        }else if (index === 6){
+          telNumber = telNumber + numbers[index] + "-";
+        }else{
+          telNumber = telNumber + numbers[index];
+        }
+      }
+    }
+    return telNumber
+  }
 }
 
 // Desafio 12
